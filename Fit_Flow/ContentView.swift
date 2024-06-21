783ef490b59f6e5bc33ea96df8b808ebf626b0f0
Thrisha Kopula <thrishak1@gmail.com>
2024-06-21 11:56:05 -0700

    Initial Commit

---
+++ Fit_Flow/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Fit_Flow
+//
+//  Created by Thrisha Kopula on 6/21/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Fit_Flow/Fit_FlowApp.swift
@@ -0,0 +1,17 @@
+//
+//  Fit_FlowApp.swift
+//  Fit_Flow
+//
+//  Created by Thrisha Kopula on 6/21/24.
+//
+
+import SwiftUI
+
+@main
+struct Fit_FlowApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
